5942b0ff5477d6621e7e6bb1dfbdbb73ba3f465a
letinv <letinv@korea.ac.kr>
2026-05-30 21:53:36 +0900

    add Vercel domain to CORS

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ from pathlib import Path
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://hci-null.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
