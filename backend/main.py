f56f6b94d02434be1ea4a8ed750d0f2daaf185c9
letinv <letinv@korea.ac.kr>
2026-06-11 20:34:09 +0900

    allow all origins for CORS

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ except Exception:
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://hci-null.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
